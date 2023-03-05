45ba34b56a299fd064979df20fc94fcb9e503ff4
SameerJadav <sameerjadav010@gmail.com>
2023-03-05 20:40:49 +0530

    Fixed typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 // import type { Metadata } from "next";
 
 // export const metadata: Metadata = {};
-export const metadata: Metadata = {
+export const metadata = {
   title: {
     default: "Quizzical",
     template: "%s | Quizzical",
